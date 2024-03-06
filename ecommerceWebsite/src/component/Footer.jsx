d5df873e94e001e2ba7e51e6024cd99c8adab9a2
Aniket Shelake <aniketshelake151@sitcoe.org.in>
2024-03-06 11:07:44 +0530

    modified footer

--- ecommerceWebsite/src/component/Footer.jsx
+++ ecommerceWebsite/src/component/Footer.jsx
@@ -6,19 +6,19 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <div className="flex justify-center items-center gap-12 sm:gap-20 mt-5 py-5 bg-[#f6f6f6] fixed bottom-0 w-full">
-      <div className="flex justify-center items-center flex-col gap-2 font-medium">
+      <div className="flex justify-center items-center flex-col gap-1 font-medium">
         <RiHomeLine className="text-xl"/>
         <Link to="/"><p className="cursor-pointer">Home</p></Link>
       </div>
-      <div className="flex justify-center items-center flex-col gap-2 font-medium">
+      <div className="flex justify-center items-center flex-col gap-1 font-medium">
         <RiShoppingCartLine className="text-xl"/>
         <Link to="/cart"><p className="cursor-pointer">Cart</p></Link>
       </div> 
-      <div className="flex justify-center items-center flex-col gap-2 font-medium">
+      <div className="flex justify-center items-center flex-col gap-1 font-medium">
         <RiHeart3Line className="text-xl"/>
        <Link to="/likedproducts"><p className="cursor-pointer">Liked</p></Link>
       </div>
-      <div className="flex justify-center items-center flex-col gap-2 font-medium">
+      <div className="flex justify-center items-center flex-col gap-1 font-medium">
         <CgProfile className="text-xl"/>
        <Link to="/profile"><p className="cursor-pointer">Profile</p></Link>
       </div>
