8c952f594446818a742969c87c7c233345231660
Aniket Shelake <aniketshelake151@sitcoe.org.in>
2024-03-06 13:09:09 +0530

    component created

---
+++ ecommerceWebsite/src/component/HomeLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Search from './Search'
+import Products from './Products'
+
+function HomeLayout() {
+  return (
+    <div>
+      <Search/>
+      <Products/>
+    </div>
+  )
+}
+
+export default HomeLayout
